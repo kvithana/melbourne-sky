cfed6b9ee9dae03351a4a2b4f23594916bc240ab
Kalana Vithana <kalanavithana1@gmail.com>
2020-10-11 19:44:52 +1100

    📷 update webcam

--- pages/api/color.ts
+++ pages/api/color.ts
@@ -8,12 +8,12 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   res.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
   try {
     const fac = new FastAverageColor()
-    const img = await loadImage('http://elwoodsc.axiscam.net:8201/axis-cgi/jpg/image.cgi')
+    const img = await loadImage('https://www.somersyc.com.au/webcams/webcam1.jpg')
     const { width, height } = img
     const canvas = createCanvas(width, height)
     const ctx = canvas.getContext('2d')
     ctx.drawImage(img, 0, 0)
-    const imageData = ctx.getImageData(0, 30, width, height / 8)
+    const imageData = ctx.getImageData(0, 30, width, height / 2)
 
     // get colour
     const colourData = await fac.getColorFromArray4((imageData.data as unknown) as Uint8Array, {
