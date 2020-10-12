3909a9965b5cfcd8a0cad1d226f1c1fe6607cd24
Kalana Vithana <kalanavithana1@gmail.com>
2020-10-13 01:00:12 +1100

    😒 fix bug in cam select logic + format api

--- pages/api/color.ts
+++ pages/api/color.ts
@@ -1,28 +1,31 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import FastAverageColor from 'fast-average-color'
-import { createCanvas, loadImage } from 'canvas'
+import { createCanvas, ImageData, loadImage } from 'canvas'
 import Color from 'color'
 import namer from 'color-namer'
 import { utcToZonedTime } from 'date-fns-tz'
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   res.setHeader('Cache-Control', 's-maxage=60')
+  const fac = new FastAverageColor()
+  const zonedDate = utcToZonedTime(new Date(), 'Australia/Melbourne')
+  let camURL: string
+  if (zonedDate.getHours() < 10) {
+    camURL = 'https://www.somersyc.com.au/webcams/webcam2.jpg'
+  } else {
+    camURL = 'https://www.somersyc.com.au/webcams/webcam1.jpg'
+  }
+
   try {
-    const fac = new FastAverageColor()
-    const zonedDate = utcToZonedTime(new Date(), 'Australia/Melbourne')
-    let camURL: string
-    if (zonedDate.getHours() < 10) {
-      camURL = 'https://www.somersyc.com.au/webcams/webcam2.jpg'
-    } else {
-      camURL = 'https://www.somersyc.com.au/webcams/webcam1.jpg'
-    }
     const img = await loadImage(camURL)
     const { width, height } = img
     const canvas = createCanvas(width, height)
     const ctx = canvas.getContext('2d')
     ctx.drawImage(img, 0, 0)
-    let imageData
-    if ((camURL = 'https://www.somersyc.com.au/webcams/webcam1.jpg')) {
+
+    // crop image
+    let imageData: ImageData
+    if (camURL == 'https://www.somersyc.com.au/webcams/webcam1.jpg') {
       imageData = ctx.getImageData(0, 200, width - 200, 100)
     } else {
       imageData = ctx.getImageData(0, 0, width, 200)
@@ -32,7 +35,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     const colourData = await fac.getColorFromArray4((imageData.data as unknown) as Uint8Array, {
       algorithm: 'simple',
     })
+
+    // saturate
     const c = new Color(colourData).saturate(0.5)
+
     res.json({ color: c.hex(), name: namer(c.hex()).pantone[0], isDark: c.isDark() })
   } catch (e) {
     res.statusCode = 500
